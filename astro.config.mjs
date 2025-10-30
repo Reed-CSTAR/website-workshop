4b3a484f3fb6e851cb1a2b290ddca7dde3f565ac
Tristan F. <pub.tristanf@gmail.com>
2025-10-29 22:32:29 -0700

    style: wee nicer code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
     markdown: {
         shikiConfig: {
             themes: {
-                light: 'github-light',
-                dark: 'github-dark',
+                light: 'github-light-high-contrast',
+                dark: 'github-dark-high-contrast',
             },
         },
     },
